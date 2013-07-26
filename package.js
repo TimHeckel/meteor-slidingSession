7bd6aa1326f078bad68097e3109701e7b1ced4fb
Tim Heckel <tim@slatebox.com>
2013-07-25 19:01:05 -0500

    Fixing api package add

--- package.js
+++ package.js
@@ -5,17 +5,10 @@ Package.describe({
 Package.on_use(function (api) {
 
     api.use(['jquery', 'underscore', 'templating'], 'client');
-    api.add_files([
-    	 'lib/shared.js'
-    	, 'lib/client/activeSessions.html'
-        , 'lib/client/activeSessions.js'
-        , 'lib/client/css/activeSessions.css'
-       
-    ], 'client');
-
-    api.add_files([
-	 	'lib/shared.js'
-    	, 'lib/server/activeSessions.js'
-    ], 'server');
+    api.add_files('lib/shared.js', ['client', 'server']);
+    api.add_files('lib/client/activeSessions.html', 'client');
+    api.add_files('lib/client/activeSessions.js', 'client');
+    api.add_files('lib/client/css/activeSessions.css', 'client');
+    api.add_files('lib/server/activeSessions.js', 'server');
 
 });
